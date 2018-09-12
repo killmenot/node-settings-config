86205f6cfda474cbe3a9105bbe20481c4b5049b1
Alexey Kucherenko <alexei.kucherenko@gmail.com>
2018-09-12 19:31:12 +0300

    refactoring

--- test/config.test.js
+++ test/config.test.js
@@ -1,95 +1,128 @@
-/* globals beforeEach, describe, it */
-
 'use strict';
 
-var path = require('path');
-var expect = require('chai').expect;
-var Config = require('../lib/config');
+const path = require('path');
+const expect = require('chai').expect;
+const Config = require('../').Config;
 
-describe('settings-config', function () {
-  var config;
+describe('settings-config', () => {
+  let config;
 
-  beforeEach(function () {
+  beforeEach(() => {
     process.chdir(path.join(__dirname, 'fixtures/app'));
     delete process.env.NODE_ENV;
     delete process.env.CONFIG_DIR;
   });
 
-  it('application.js', function () {
+  it('config dir does not exist', () => {
+    expect(() => {
+      config = new Config('/foo/bar/baz');
+    }).throw(Error, 'The config dir does not exists.');
+  });
+
+  it('application.js', () => {
     process.chdir(path.join(__dirname, 'fixtures/application-js'));
+
     config = new Config();
+
     expect(config.value).to.equal('application-js');
   });
 
-  it('application.json', function () {
+  it('application.json', () => {
     process.chdir(path.join(__dirname, 'fixtures/application-json'));
+
     config = new Config();
+
     expect(config.value).to.equal('application-json');
   });
 
-  it('no application file', function () {
+  it('no application file', () => {
     process.chdir(path.join(__dirname, 'fixtures/no-application'));
+
     config = new Config();
+
     expect(config).to.be.an('object');
   });
 
-  it('basic', function () {
+  it('basic', () => {
     config = new Config();
+
     expect(config.value).to.equal('production');
     expect(config.foo).to.equal('bar');
   });
 
-  it('external storage feature', function () {
+  it('external storage feature', () => {
     process.env.NODE_ENV = 'develop';
+
     config = new Config();
+
     expect(config.value).to.equal('develop');
     expect(config.foo).to.equal('bar');
   });
 
-  it('using env.NODE_ENV', function () {
-    process.env.NODE_ENV = 'test';
+  it('external storage feature (external values do not override file system values)', () => {
+    process.env.NODE_ENV = 'master';
+
     config = new Config();
+
+    expect(config.value).to.equal('master');
+  });
+
+  it('using env.NODE_ENV', () => {
+    process.env.NODE_ENV = 'test';
+
+    config = new Config();
+
     expect(config.value).to.equal('test');
     expect(config.foo).to.equal('bar');
   });
 
-  it('using env.CONFIG_DIR with absolute path', function () {
+  it('using env.CONFIG_DIR with absolute path', () => {
     process.env.NODE_ENV = 'staging';
     process.env.CONFIG_DIR = path.join(__dirname, 'fixtures/app/config');
+
     config = new Config();
+
     expect(config.value).to.equal('staging');
     expect(config.foo).to.equal('bar');
   });
 
-  it('using env.CONFIG_DIR with relative path', function () {
+  it('using env.CONFIG_DIR with relative path', () => {
     process.chdir(path.join(__dirname));
 
     process.env.NODE_ENV = 'staging';
     process.env.CONFIG_DIR = 'fixtures/app/config';
+
     config = new Config();
+
     expect(config.value).to.equal('staging');
     expect(config.foo).to.equal('bar');
   });
 
-  it('using env.EXTERNAL_STORAGE', function () {
+  it('using env.EXTERNAL_STORAGE', () => {
     process.env.NODE_ENV = 'develop';
     process.env.EXTERNAL_STORAGE = path.join(__dirname, 'fixtures/server.json');
+
     config = new Config();
+
     expect(config.value).to.equal('develop');
     expect(config.foo).to.equal('bar');
   });
 
-  it('using env.NODE_ENV for non existing configuration', function () {
+  it('using env.NODE_ENV for non existing configuration', () => {
     process.env.NODE_ENV = 'foobar';
+
     config = new Config();
+
     expect(config.value).to.equal('production');
     expect(config.foo).to.equal('bar');
   });
 
-  it('using consturctor parameters', function () {
-    var configDir = path.join(__dirname, 'fixtures/app/config');
-    var env = 'staging';
+  it('using consturctor parameters', () => {
+    const configDir = path.join(__dirname, 'fixtures/app/config');
+    const env = 'staging';
+
     config = new Config(configDir, env);
+
     expect(config.value).to.equal('staging');
     expect(config.foo).to.equal('bar');
   });
